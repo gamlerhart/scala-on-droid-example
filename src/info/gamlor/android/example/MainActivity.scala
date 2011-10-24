b5b0c980383c4efab4fc44c38eaaf94c0f07a47e
Roman Stoffel <roman.stoffel@gamlor.info>
2011-10-24 19:09:00 +0200

    example is working?

--- src/info/gamlor/android/example/MainActivity.scala
+++ src/info/gamlor/android/example/MainActivity.scala
@@ -2,12 +2,20 @@ package info.gamlor.android.example
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity extends Activity {
   /**Called when the activity is first created. */
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
+
+
+  }
+
+  def sayHelloFromScala(){
+    val textView = findViewById(R.id.helloWorld).asInstanceOf[TextView]
+    textView.setText("Hello from Scala")
   }
 }
 
